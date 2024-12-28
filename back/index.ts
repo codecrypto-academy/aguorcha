831e085e0dd750e6e8719ac650206e4bb00d442a
Agu Orcha <aguorcha@gmail.com>
2024-12-28 18:02:16 +0100

    Rutas de balance, con petición RPC directa al nodo y con la librería ethers

--- back/index.ts
+++ back/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Request, Response } from "express";
 import cors from 'cors';
+import { ethers } from "ethers";
 
 const app = express();
 app.use(express.json());
@@ -8,18 +9,38 @@ app.use(cors());
 
 const port = 3333;
 
-app.get('/:p1/:p2', (req: Request, res: Response) => {
-  const {p1, p2} = req.params;
-  res.send(
-    {p1:p1, p2:p2}
+app.get("/api/balanceEthers/:address", async (req: Request, res: Response) => {
+  const { address } = req.params;
+  const provider = new ethers.JsonRpcProvider('http://localhost:5556/');
+  const balance = await provider.getBalance(address);
+  res.json(
+    { address, balance: Number(balance) / 10 ** 18, fecha: new Date().toISOString() }
   );
 })
 
-app.post('/', (req: Request, res: Response) => {
-  const body = req.body;
-  res.send(
-    body
-  );
+app.get("/api/balance/:address", async (req: Request, res: Response) => {
+  const { address } = req.params;
+  const retorno = await fetch('http://localhost:5556', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'eth_getBalance',
+      params: [
+        address,
+        'latest'
+      ],
+      id: 1
+    })
+  })
+  const data = await retorno.json();
+  res.json({
+    address,
+    balance: Number(data.result) / 10 ** 18,
+    fecha: new Date().toISOString()
+  });
 })
 
 app.listen(port, () => {
