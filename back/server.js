c0ecea5bab474c6ec42f828513e2ee142ccf7723
Agu Orcha <aguorcha@gmail.com>
2025-01-04 19:01:29 +0100

    Backend de la aplicación con la lógica para traer los productos de la base de datos

---
+++ back/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const db = require("./db");
+
+app.listen(5555);
+
+app.get("/ping", (req, res) => {
+  res.send({ fecha: new Date() });
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const [results, fields] = await db.q("select * from Products", []);
+    res.send(results);
+  } catch (error) {
+    res.send({ error })
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const [results, fields] = await db.q("select * from Products where ProductID = ?", [req.params.id]);
+    res.send(results);
+  } catch (error) {
+    res.send({ error })
+  }
+});
