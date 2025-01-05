dd333bc60e25489b6a9e93de4c938336f897e38f
Agu Orcha <aguorcha@gmail.com>
2025-01-05 11:39:36 +0100

    Creación del estado y maquetación del componente Producto

--- front/src/components/Producto.jsx
+++ front/src/components/Producto.jsx
@@ -1,15 +1,36 @@
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
 
 export function Producto() {
   const params = useParams();
 
-  const {data, isLoading} = useQuery("producto", () => {
-    return fetch(`http://localhost:5555/products/${params.id}`)
-    .then(res => res.json());
+  const { data, isLoading } = useQuery("producto", () => {
+    return fetch(`http://localhost:5555/products/${params.id}`).then((res) =>
+      res.json()
+    );
   });
   if (isLoading) {
-    return <div>Cargando...</div>
+    return <div>Cargando...</div>;
   }
-  return <p>{JSON.stringify(data)}</p>
+  return (
+    <div>
+      <h3>Producto</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <td>{data[0].ProductID}</td>
+          </tr>
+          <tr>
+            <th>Nombre</th>
+            <td>{data[0].ProductName}</td>
+          </tr>
+          <tr>
+            <th>Precio</th>
+            <td>{data[0].UnitPrice}</td>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,29 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { Home } from './components/Home';
-import { Productos } from './components/Productos';
-import { Producto } from './components/Producto';
-import { Cesta } from './components/Cesta';
-import './index.css'
+import { StrictMode, createContext, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Home } from "./components/Home";
+import { Productos } from "./components/Productos";
+import { Producto } from "./components/Producto";
+import { Cesta } from "./components/Cesta";
+import "./index.css";
 
 const queryClient = new QueryClient();
+const Context = createContext(null);
 
-createRoot(document.getElementById('root')).render(
+function App() {
+  const [estado, setEstado] = useState({
+    cesta: [],
+  });
+
+  return (
+    <Context.Provider value={[estado, setEstado]}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route index element={<Productos />} />
+              <Route path="*" element={<Productos />} />
+              <Route path="productos" element={<Productos />} />
+              <Route path="productos/:id" element={<Producto />} />
+              <Route path="cesta" element={<Cesta />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Context.Provider>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}>
-            <Route index element={<Productos />} />
-            <Route path='*' element={<Productos />} />
-            <Route path='productos' element={<Productos />} />
-            <Route path='productos/:id' element={<Producto />} />
-            <Route path='cesta' element={<Cesta />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <App />
   </StrictMode>
-)
+);
