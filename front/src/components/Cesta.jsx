fee7d75b2be38ba40a1d5c48965dee3077ae02e1
Agu Orcha <aguorcha@gmail.com>
2025-01-06 11:46:37 +0100

    Componente Cesta

--- front/src/components/Cesta.jsx
+++ front/src/components/Cesta.jsx
@@ -1,3 +1,35 @@
+import { useContext } from "react";
+import { Context } from "../main";
+
 export function Cesta() {
-  return <h2>Cesta</h2>
+  const [estado, setEstado] = useContext(Context);
+  const total = estado.cesta.reduce((acc, item) => acc + item.total, 0);
+
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {estado.cesta.map((i) => (
+            <tr key={i.producto.ProductID}>
+              <td>{i.producto.ProductID}</td>
+              <td>{i.producto.ProductName}</td>
+              <td>{i.producto.UnitPrice}</td>
+              <td>{i.cantidad}</td>
+              <td>{i.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>Total {total}</h3>
+    </div>
+  );
 }
