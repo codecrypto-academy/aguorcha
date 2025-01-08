45ec830a912302dda0e8580aa8311ce8ca015b56
Agu Orcha <aguorcha@gmail.com>
2025-01-08 18:42:34 +0100

    Componente de precios

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,3 +1,33 @@
+import data from "../data.json";
+
+function CardPrice({ data }) {
+  return (
+    <div className="card w-100 m-1">
+      <div className="card-header">
+        <h4 className="text-center">{data.titulo}</h4>
+      </div>
+      <div className="card-body">
+        <ul className="list-unstyled">
+          {data.features.map((i, index) => (
+            <li key={index} className="mt-1">
+              {i}
+            </li>
+          ))}
+        </ul>
+        <button className="btn btn-lg btn-outline-primary w-100">
+          {data.textBoton}
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export function Prices() {
-  return <div>Precios</div>;
+  return (
+    <div className="my-8 d-flex justify-content-between">
+      {data.precios.map((i, index) => (
+        <CardPrice data={i} key={index}></CardPrice>
+      ))}
+    </div>
+  );
 }
