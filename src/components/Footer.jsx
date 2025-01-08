bac26acfd0d4bbf4650a6e31dca53d9fccdf52f6
Agu Orcha <aguorcha@gmail.com>
2025-01-08 18:09:35 +0100

    Componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,34 @@
+import data from "../data.json";
+
+function Section({ data }) {
+  return (
+    <div>
+      <h5>{data.titulo}</h5>
+      <ul className="nav flex-column">
+        {data.links.map((item, index) => (
+          <li key={index}>
+            <a
+              className="text-decoration-none"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.url}
+            >
+              {item.titulo}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="d-flex justify-content-between mt-4">
+      <div className="fs-4">{data.header.nombre}</div>
+      {data.footer.map((item, index) => (
+        <Section key={index} data={item} />
+      ))}
+    </div>
+  );
 }
