f373f2b7688ef01d4acb3274889c742d802a5899
Agu Orcha <aguorcha@gmail.com>
2025-01-14 18:19:38 +0100

    Rutas del backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
-
+const { Web3 } = require("web3");
+require('dotenv').config();
 const app = express();
+const web3 = new Web3(process.env.URL_INFURA);
 
 app.get("/", async (req, res) => {
-  res.send("hola");
+  const bloque = await web3.eth.getBlockNumber();
+  res.send({ bloque });
 })
 
-app.listen(3333);
+app.get("/bloque/:bloque", async (req, res) => {
+  const bloque = await web3.eth.getBlock(req.params.bloque);
+  res.send(bloque);
+})
+
+app.get("/tx/:tx", async (req, res) => {
+  const tx = await web3.eth.getTransaction(req.params.tx);
+  res.send(tx);
+})
+
+app.get("/balance/:address", async (req, res) => {
+  const balance = await web3.eth.getBalance(req.params.address);
+  res.send({
+    balance,
+    ethers: web3.utils.fromWei(balance, 'ether')
+  });
+})
+
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
+app.listen(3333, () => {
+  console.log("Servidor escuchando en http://localhost:3333");
+});
